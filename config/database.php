0873a94492118855d6fa2511e1a1ce4f07fcfeb4
renaudbillen <renaudbillen@gmail.com>
2026-01-15 22:58:28 +0100

    mysql ssl handle

--- config/database.php
+++ config/database.php
@@ -61,6 +61,8 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
+            'encrypt' => 'yes',
+            'trust_server_certificate' => true,
         ],
 
         'mariadb' => [
@@ -148,7 +150,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
+            'prefix' => env('REDIS_PREFIX', Str::slug((string)env('APP_NAME', 'laravel')) . '-database-'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
